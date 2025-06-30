397f1462c9157b4536e693b827a7949f9827a965
Tahanaz <tahanaz8242@gmail.com>
2025-06-30 17:46:17 +0500

    responsiveness

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -3,51 +3,42 @@ import "./login.css";
 import { useState } from "react";
 
 export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
+  const [username , setUsername] =useState('');
+  const [password, setPassword] =useState('')
+  const handleSubmit =(e) =>{
     e.preventDefault();
-    if (username.trim() === "" || password.trim() === "") {
-      alert("please fill the both field");
-      return;
+    if (username.trim()==='' || password.trim()===''){
+            alert('please fill the both field')
+            return;
     }
-    alert(`welcome, ${username}`);
-  };
+    alert(`welcome, ${username}`)
+  }
   return (
     <>
       <div className="parent">
         <div className="first-container">
-          <div>
-            <img src="/image/icon1.png" alt="Logo" />
-          </div>
-          <div>
-            <h1>MYSTURA</h1>
-            <p>FOR THE PEOPLE BY THE PEOPLE</p>
-          </div>
+          <img src="/image/icon1.png" alt="Logo" />
+          <h1 className="heading">MYSTURA</h1>
+          <p className="pera">FOR THE PEOPLE BY THE PEOPLE</p>
         </div>
-
         <div className="second-container">
-          <h2 className="heading">Welcome Back to Home MYSTURA</h2>
-          <p className="pera">
-            Enter your login details to continue exploring smart home &
-            lifestyle services with Mystura.
-          </p>
+         
           <form className="form" onSubmit={handleSubmit}>
-            <span>Username</span>
+             <h2 className="heading">Welcome Back to Home MYSTURA</h2>
+          <p className="pera">Enter your login details to continue exploring smart home & lifestyle services with Mystura.</p>
             <input
-              className="input"
               type="text"
               placeholder="Username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
+              // style={styles.input}
             />
-            <span>Password</span>
             <input
-              className="input"
               type="password"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              // style={styles.input}
             />
             <button type="submit">Login</button>
           </form>
