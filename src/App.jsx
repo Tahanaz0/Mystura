6ade864faca9da696f0dc04bd4d3d11fbe4c77ca
Tahanaz <tahanaz8242@gmail.com>
2025-07-12 19:31:23 +0500

    'test'

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Login from './component/login/Login';
-import Dashboard from './component/dashboard/Dashboard';
-import SideBar2 from './component/sideBar2/SideBar2';
-// import Header from './component/Header/Header';
-import Header2 from './component/Header2/Header2';
+import Dashboard from './component/dashboard/dashboard';
+import SideBar from './component/sideBar/sideBar';
+import Header from './component/Header/Header';
+// import Header2 from './component/Header2/Header2';
 
 function LayoutWithHeaderSidebar({ children }) {
   return (
     <>
-      <Header2 />
+      <Header />
       <div style={{ display: 'flex' }}>
-        <SideBar2/>
+        <SideBar/>
         <div className="main-content">
           {/* {children} */}
         </div>
